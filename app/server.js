0f6ed658a02dbf56be2a4b2994481913a15add24
RanarTheWrathful <143533828+RanarTheWrathful@users.noreply.github.com>
2025-10-26 17:56:45 -0400

    Update server.js

--- app/server.js
+++ app/server.js
@@ -18994,7 +18994,8 @@ if (n.type === "atmosphere"||(n.repairEffect||n.healEffect) && !n.isProjectile) 
     if (n.health.amount >= n.health.max) n.shield.amount += n.shield.max/4;
   }
   } else {
-  my.damageRecieved += (damage._n * deathFactor._n) * n.factor;
+  if (my.type === "atmosphere" && !n.isProjectile||n.type === "atmosphere" && !my.isProjectile) return;
+ my.damageRecieved += (damage._n * deathFactor._n) * n.factor;
   n.damageRecieved += (damage._me * deathFactor._me) * my.factor; 
   }
 } catch (err) {
