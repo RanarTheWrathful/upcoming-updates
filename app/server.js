c9f06e5a5348ee52286c2740c3632a488d200d5a
RanarTheWrathful <143533828+RanarTheWrathful@users.noreply.github.com>
2025-10-26 12:13:38 -0400

    Update server.js

--- app/server.js
+++ app/server.js
@@ -63,7 +63,7 @@ let modeList = ["Unknown"];
 let serverType = "testing"; //change this to play preset modes look
 if (serverType === "JJ's Reasearch Facility")
   chosenMode = "JJ's Reasearch Facility"; //or this
-if (serverType === "testing") chosenMode = "SandboxWD";
+if (serverType === "testing") chosenMode = "The Infestation";
 //change this to play a specifict mode
 else if (serverType === "normal") {
   //dont change these - J.J.
