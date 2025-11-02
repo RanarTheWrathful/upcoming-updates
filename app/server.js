bca3afc4bfa92c3b5a5613b7672fb528e5591867
RanarTheWrathful <143533828+RanarTheWrathful@users.noreply.github.com>
2025-11-02 14:24:36 -0500

    Update server.js

--- app/server.js
+++ app/server.js
@@ -360,7 +360,7 @@ if (c.MODE === "theDistance") c.startingClass = "racer";
 if (c.MODE === "theRestless") c.startingClass = "highlordLegendaryClasses";
 if (c.MODE === "theAwakening") c.startingClass = "arenaguardpl";
 if (c.MODE === "theAssault") c.startingClass = "swarmProtector2";
-if (serverType === "testing") c.startingClass = "testbed";
+if (serverType === "testin") c.startingClass = "testbed";
 c.DomxClass = "minibois";
 function removeMuted(socketIP) {
   console.log("Removing from muteList:", socketIP);
